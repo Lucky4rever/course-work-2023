bca6a33d0d8a03e26edc124ba8714a466103357e
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-05-23 12:35:13 +0300

    First commit

---
+++ client/src/components/CreateRows/index.ts
@@ -0,0 +1,3 @@
+export { default as CreateEmployee } from "./CreateEmployee";
+export { default as CreateCollector } from "./CreateCollector";
+export { default as CreateItinerary } from "./CreateItinerary";
